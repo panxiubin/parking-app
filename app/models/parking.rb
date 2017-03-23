55187faffd84bd4f2421adf0f63abbb639c8e4f1
panxiubin <xiubinpan@gmail.com>
2017-03-23 22:44:26 +0800

    implement guest

--- app/models/parking.rb
+++ app/models/parking.rb
@@ -14,4 +14,39 @@ class Parking < ApplicationRecord
       errors.add(:end_at, "有金额就必须有结束时间")
     end
   end
+
+  # 计算停了多少分钟
+  def duration
+    ( end_at - start_at ) / 60
+  end
+
+  # def calculate_amount
+  #   # 如果有开始时间和结束时间，则可以计算价格
+  #   if self.amount.blank? && self.start_at.present? && self.end_at.present?
+  #     # if duration <= 60
+  #     #   self.amount = 200
+  #     # end
+  #     total = 0
+  #     if duration <= 60
+  #       total = 200
+  #     else
+  #       total += 200
+  #       left_duration = duration - 60
+  #       total += ( left_duration.to_f / 30 ).ceil * 100
+  #     end
+  #
+  #     self.amount = total
+  #   end
+  # end
+
+  def calculate_amount
+    if self.amount.blank? && self.start_at.present? && self.end_at.present?
+      if duration <= 60
+        self.amount = 200
+      else
+        self.amount = 200 + ((duration - 60).to_f / 30).ceil * 100
+      end
+    end
+  end
+  
 end
