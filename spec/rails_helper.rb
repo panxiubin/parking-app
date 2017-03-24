929ddeaaf3064110e4e585b5071ab99c5f6283fe
panxiubin <xiubinpan@gmail.com>
2017-03-24 17:06:09 +0800

    Add Rspec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,9 +5,6 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'spec_helper'
 require 'rspec/rails'
-
-require 'capybara/rails'
-require 'capybara/rspec'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -57,10 +54,4 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
-  # config.filter_run :focus => true
-  # config.run_all_when_everything_filtered = true
-
-  config.include Devise::Test::ControllerHelpers, type: :ControllerHelpers
-  config.include Devise::Test::ControllerHelpers, type: :view
-  config.include Devise::Test::IntegrationHelpers, type: :feature
 end
